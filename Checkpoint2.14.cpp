aa9e4728566806fc66b0c40d8b3dfb6d69cad53e
Ebram19 <ebramazer810@icloud.com>
2024-09-13 15:59:13 -0400

    Update Checkpoint2.14.cpp

--- Checkpoint2.14.cpp
+++ Checkpoint2.14.cpp
@@ -6,7 +6,7 @@ first, middle, and last.Store your initials in these variables
 then display them on the screen
 */
 #include <iostream>
-
+#iclude <string>
 int main()
 {
 char firstInitial;
@@ -18,7 +18,11 @@ std::cout << "Please enter the initial of your middle name:";
 std::cin >> middleInitial;
 std::cout << "Please enter the initial of your last name:";
 std::cin >> lastInitial;
-std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial;
+std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial;  
+std::cout <<"Please enter your first name:";
+std::cin >> firstName;
+std::cout << "Your first name is" <<firstName:
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
